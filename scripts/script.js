f55167fa901b9841c377f98d6542702d0fea3e66
masonw2466-ops <masonw2466@gmail.com>
2025-11-18 12:39:24 -0700

    JS nav

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,23 @@ Class: CSCY 2000 Web Development
 */
 
 // Example: simple script to toggle a message
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("NOAM Cybersecurity site loaded successfully.");
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("navigation-placeholder").innerHTML = `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.html">NOAM Cybersecurity</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="learn.html">Learn</a></li>
+            <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
+            <li class="nav-item"><a class="nav-link" href="portfolio-brian.html">Brian's Portfolio</a></li>
+            <li class="nav-item"><a class="nav-link" href="sales.html">Sales</a></li>
+            <li class="nav-item"><a class="nav-link" href="location.html">Location</a></li>
+            <li class="nav-item"><a class="nav-link" href="informative.html">Informative</a></li>
+        </ul>
+    </div>
+  </nav>
+  `;
 });
